e46d9b2c5bda206fc09e109540194ecc76a66838
kyb <i.kyb@ya.ru>
2020-11-06 17:22:49 +0200

    amalgamate + rev-label

--- amalgamate.sh
+++ amalgamate.sh
@@ -12,8 +12,13 @@ rm_once(){
    sed -E '/^#pragma once/'d "$@"
 }
 
+SCRIPT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
+
 cat >"$OUT" <<END
 #pragma once
+
+#define BYTE_VECTOR_REV_LABEL "$($SCRIPT_DIR/git-rev-label-short.sh)"
+#define BYTE_VECTOR_VERSION BYTE_VECTOR_REV_LABEL
 
 /////////////  byte_vector.hpp  /////////////////
 $(rm_once byte_vector.hpp)
--- git-rev-label-short.sh
+++ git-rev-label-short.sh
@@ -11,7 +11,8 @@ _DIRTY=${_dirty^^};
 tag="$(git tag --list --points-at HEAD | head -1)";
 branch="$(git rev-parse --abbrev-ref HEAD | sed s,^HEAD$,DETACHED,)";
 refname=$(if test "$branch" == DETACHED; then echo "${tag:-DETACHED}"; else echo "$branch";fi;)
-export refname count short _DIRTY
-echo '#define REV_LABEL "$refname-c$count-g$short$_DIRTY"' | { 
-   perl -pe'$var="[A-Za-z_][A-Za-z0-9_]+"; s,\$(?:($var)|\{($var)\}),$ENV{$1//$2}//$&,eg' 
-} >rev-label.h
+# export refname count short _DIRTY
+# echo '#define BYTE_VECTOR_REV_LABEL "$refname-c$count-g$short$_DIRTY"' | { 
+#   perl -pe'$var="[A-Za-z_][A-Za-z0-9_]+"; s,\$(?:($var)|\{($var)\}),$ENV{$1//$2}//$&,eg' 
+# } >rev-label.h
+echo "$refname-c$count-g$short$_DIRTY"
